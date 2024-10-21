4fd3908f30fa06a368caf83e89386dabe54740c0
coderisliev07 <coderbhai07@gmail.com>
2024-10-21 13:26:37 +0530

    my portfolio

---
+++ src/Pages/home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Hero from '../Components/hero'
+
+
+export default function Home() {
+  return (
+    <div className='pl-36 pr-36'>
+    <Hero/>
+      
+    </div>
+  )
+}
