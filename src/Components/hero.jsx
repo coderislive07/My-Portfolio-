111ac1e03979701c5bf35ce5790a48834e5b645c
coderisliev07 <coderbhai07@gmail.com>
2024-10-21 15:00:21 +0530

    added close icon

--- src/Components/hero.jsx
+++ src/Components/hero.jsx
@@ -24,7 +24,7 @@ export default function Hero() {
                 >
                     <img src={bar} alt="Toggle Menu" className="w-full h-full invert" />
                 </button>
-                <div className={`fixed top-0 left-0 h-full w-64 bg-black z-50 transform transition-transform duration-300 ease-in-out ${isOpen ? 'translate-x-0' : '-translate-x-full'} sm:static sm:h-auto sm:w-auto sm:bg-transparent sm:transform-none sm:transition-none sm:flex sm:items-center`}>
+                <div className={`fixed flex flex-row top-0 left-0 h-full w-64 bg-black z-50 transform transition-transform duration-300 ease-in-out ${isOpen ? 'translate-x-0' : '-translate-x-full'} sm:static sm:h-auto sm:w-auto sm:bg-transparent sm:transform-none sm:transition-none sm:flex sm:items-center`}>
                     <div className="flex flex-col h-full p-4 sm:flex-row sm:p-0">
                         <a href="#" className="flex items-center mb-4 sm:mb-0 sm:mr-4">
                             <img src={home} alt="Home" className="w-5 h-5 mr-1 invert" />
@@ -41,6 +41,12 @@ export default function Hero() {
                         <button className="bg-transparent text-purple-700 border text-sm sm:text-lg border-purple-700 px-4 py-2 rounded-full hover:text-white hover:bg-purple-700 transition-colors duration-300">
                             Contact
                         </button>
+                    </div>
+                    <div className='ml-[22vw] mt-[2vw]'>
+                    <svg onClick={toggleNavbar} class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+  <path stroke="white" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m15 9-6 6m0-6 6 6m6-3a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
+</svg>
+
                     </div>
                 </div>
             </nav>
